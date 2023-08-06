f48cf02fafadc99e7424103f27cfa22a43809341
semperprimum <bkeem0266+github@gmail.com>
2023-08-06 15:49:36 +0600

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import { Footer } from "../../components/Footer.styled.jsx";
-import { Grid } from "./components/Grid.styled.jsx";
 import { Hero } from "./components/Hero.styled.jsx";
+import { Grid } from "./components/Grid.styled.jsx";
 import { Introduction } from "./components/Introduction.styled.jsx";
 export function Home() {
   return (
